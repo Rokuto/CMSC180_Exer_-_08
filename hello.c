40fce1e5b3d4bd82a8d4345afe443d58902f2084
user <user@node25.(none)>
2016-11-07 17:31:48 +0800

    test

--- hello.c
+++ hello.c
@@ -3,38 +3,9 @@
 #include <stdlib.h>
 
 
-/*int *createMatrix (int nrows, int ncols) {
-    int *matrix, row[nrows][ncols];
-    int h = 0, i, j;
-
-    if (( matrix = malloc(nrows*ncols*sizeof(int))) == NULL) {
-        printf("Malloc error");
-        exit(1);
-    }
-
-    for(i = 0; i < nrows; i++){
-        for(j = 0; j < ncols; j++){
-            row[i][j] = 1;
-            //printf("%d ", row[i][j]);
-            h++;
-        }
-        //printf("\n");
-    }
-
-    h = 0;
-    for(i = 0; i < nrows; i++){
-        for(j = 0; j < ncols; j++){
-            matrix[h] = row[j][i];
-            h++;
-        }
-    }
-
-    return matrix;
-}*/
-
 int main(int argc, char **argv) {
     int size, rank;
-    int n = 12000, root = 0;
+    int n = 10000, root = 0;
     int i = 0;
 
     MPI_Init(&argc, &argv);
@@ -43,26 +14,21 @@ int main(int argc, char **argv) {
 
     int *globaldata = NULL;
     int *localdata = NULL;
-    //int *localsum = NULL;
-    //int *globalsum = NULL;
 
-    //localsum = malloc(sizeof(int) * n/size);
-    localdata = malloc(sizeof(int) * n*n/size);
+    if (( localdata = malloc(sizeof(int) * n*n/size) ) == NULL) {
+        printf("Malloc error");
+        exit(1);
+    }
 
     if (rank == 0) {
-        //globaldata = createMatrix(n, n);
-        if (( globaldata = malloc(n*n*sizeof(int))) == NULL) {
+        if (( globaldata = malloc(n*n*sizeof(int) )) == NULL) {
             printf("Malloc error");
             exit(1);
         }
         
-        //globalsum = malloc(sizeof(int) * n);
-
-        //printf("Processor %d has data: ", rank);
-        /*for (i = 0; i < n * n; i++){
-            printf("%d ", globaldata[i]);
+        for (i = 0; i < n * n; i++){
+            globaldata[i] = 1;
         }
-        printf("\n");*/
     }
 
     /* Scatter */
@@ -70,18 +36,11 @@ int main(int argc, char **argv) {
 
     /* Column sums */
     for (i = n/size; i < n*n/size; ++i){
-        //printf("Processor %d has data %d\n", rank, localdata[i]);
         localdata[i] = localdata[i] + localdata[i-(n/size)];
     }
-    /* Print answer 
-    // for (int i = 0; i < n*n/size; ++i){
-    //     printf("Processor %d doubling the data, now has %d\n", rank, localdata[i]);
-    // }*/
 
     /* Get the last row */
     for (i = 0; i < n/size; ++i){
-        //printf("Processor %d data, now has %d\n", rank, localdata[(n*n/size)-i-1]);
-        //localsum[i] = localdata[(n*n/size)-i-1];
         localdata[i] = localdata[(n*n/size)-i-1];
     }
 
@@ -95,16 +54,11 @@ int main(int argc, char **argv) {
             printf("%d ", globaldata[i]);
         }
         printf("\n");
+        free(globaldata);
     }
 
-    if (rank == 0){
-        free(globaldata);
-        //free(globalsum);
-    }
     free(localdata);
 
-    //free(localsum);
-    //free(localdata);
     MPI_Finalize();
     return 0;
 }
