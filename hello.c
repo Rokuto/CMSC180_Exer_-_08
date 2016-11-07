14829bfa47bc49905919a72ae9bf9c3d5c211a10
Rokuto <gauven.boro@gmail.com>
2016-11-07 03:37:11 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -34,7 +34,7 @@ int *createMatrix (int nrows, int ncols) {
 
 int main(int argc, char **argv) {
     int size, rank;
-    int n = 1200, root = 0;
+    int n = 12000, root = 0;
     int i = 0;
 
     MPI_Init(&argc, &argv);
@@ -44,14 +44,14 @@ int main(int argc, char **argv) {
     int *globaldata = NULL;
     int *localdata = NULL;
     //int *localsum = NULL;
-    int *globalsum = NULL;
+    //int *globalsum = NULL;
 
     //localsum = malloc(sizeof(int) * n/size);
     localdata = malloc(sizeof(int) * n*n/size);
 
     if (rank == 0) {
         globaldata = createMatrix(n, n);
-        globalsum = malloc(sizeof(int) * n);
+        //globalsum = malloc(sizeof(int) * n);
 
         //printf("Processor %d has data: ", rank);
         /*for (i = 0; i < n * n; i++){
@@ -82,19 +82,19 @@ int main(int argc, char **argv) {
 
     
     /* Gather */
-    MPI_Gather(localdata, n/size, MPI_INT, globalsum, n/size, MPI_INT, root, MPI_COMM_WORLD);
+    MPI_Gather(localdata, n/size, MPI_INT, globaldata, n/size, MPI_INT, root, MPI_COMM_WORLD);
 
     if (rank == 0) {
         printf("Processor %d has sums: ", rank);
         for (i = 0; i < n; i++){
-            printf("%d ", globalsum[i]);
+            printf("%d ", globaldata[i]);
         }
         printf("\n");
     }
 
     if (rank == 0){
         free(globaldata);
-        free(globalsum);
+        //free(globalsum);
     }
     free(localdata);
 
